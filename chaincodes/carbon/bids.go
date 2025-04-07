1d328ba9be31f8aef7fe24829afdb3a15f351cf1
Johann Westphall <johannwestphall@gmail.com>
2025-04-07 01:42:42 -0300

    make generic functions to insert (pvt)data on ledger
    for now, only data with composite keys

--- chaincodes/carbon/bids.go
+++ chaincodes/carbon/bids.go
@@ -1,23 +1,31 @@
 package carbon
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/hyperledger/fabric-chaincode-go/shim"
 )
 
 // TODO: this may be a float64 passed to the chaincode via transient data
 // PrivatePrice is an for-the-government-only price encoded as a base64 string.
-type PrivatePrice string
+type PrivatePrice struct {
+	Price float64  `json:"price"`
+	BidID []string `json:"bidID"`
+}
 
 // BuyBid represents an ask from a buyer.
 // Their ID could be either x509 or pseudonym-based
 type BuyBid struct {
-	AskPrice    PrivatePrice `json:"askPrice"`
+	ID          []string     `json:"id"`
+	AskPriceID  PrivatePrice `json:"askPrice"`
 	AskQuantity float64      `json:"askQuantity"`
 	BuyerID     Identity     `json:"buyerID"`
 }
 
 type SellBid struct {
-	AskPrice    PrivatePrice `json:"askPrice"`
+	ID          []string     `json:"id"`
+	AskPriceID  PrivatePrice `json:"askPrice"`
 	AskQuantity float64      `json:"askQuantity"`
 	PropertyID  uint64       `json:"propertyID"`
 }
@@ -25,22 +33,45 @@ type SellBid struct {
 const (
 	PVT_DATA_COLLECTION = "privateDataCollection"
 	BUY_BID_PREFIX      = "buyBid"
+	BUY_BID_PVT         = "buyBidPvt"
 	SELL_BID_PREFIX     = "sellBid"
+	SELL_BID_PVT        = "sellBidPvt"
 )
 
 func PublishBuyBid(quantity float64, buyerID Identity, stub shim.ChaincodeStubInterface) error {
-	buyBid := &BuyBid{
-		AskPrice:    price,
-		AskQuantity: quantity,
-		BuyerID:     buyerID,
-	}
-	price, err := getTransientData(stub, "price")
+	priceBytes, err := getTransientData(stub, "price")
 	if err != nil {
 		return err
 	}
 
-	stub.PutPrivateData(PVT_DATA_COLLECTION, "buyBid", []byte(buyBid.AskPrice))
-	bidKey := stub.CreateCompositeKey(BUY_BID_PREFIX, []string{string(buyBid.AskPrice), string(buyBid.AskQuantity)})
-	// mocks.ChaincodeStub.
-	return buyBid
+	price, err := strconv.ParseFloat(string(priceBytes), 64)
+	if err != nil {
+		return fmt.Errorf("could not parse price: %v", err)
+	}
+
+	bidTS, err := stub.GetTxTimestamp()
+	if err != nil {
+		return fmt.Errorf("could not get transaction timestamp: %v", err)
+	}
+
+	bidID := []string{buyerID.String(), bidTS.String()}
+
+	privatePrice := &PrivatePrice{
+		Price: float64(price),
+		BidID: bidID,
+	}
+
+	if err := putPvtDataWithCompositeKey[*PrivatePrice](stub, BUY_BID_PREFIX, bidID, PVT_DATA_COLLECTION, privatePrice); err != nil {
+		return err
+	}
+
+	buyBid := &BuyBid{
+		ID:          bidID,
+		AskQuantity: quantity,
+		BuyerID:     buyerID,
+	}
+	if err := putStateWithCompositeKey[*BuyBid](stub, BUY_BID_PREFIX, bidID, buyBid); err != nil {
+		return err
+	}
+	return nil
 }
--- chaincodes/carbon/chaincode_utils.go
+++ chaincodes/carbon/chaincode_utils.go
@@ -17,3 +17,33 @@ func getTransientData(stub shim.ChaincodeStubInterface, key string) ([]byte, err
 	}
 	return transientField, nil
 }
+
+func putPvtDataWithCompositeKey[T any](stub shim.ChaincodeStubInterface, objectType string, keyAttributes []string, collectionName string, pvtDataStruct T) error {
+	pvtDataKey, err := stub.CreateCompositeKey(objectType, keyAttributes)
+	if err != nil {
+		return fmt.Errorf("could not create composite key for pvt data: %v", err)
+	}
+	pvtDataBytes, err := json.Marshal(pvtDataStruct)
+	if err != nil {
+		return fmt.Errorf("could not marshal private data: %v", err)
+	}
+	if err := stub.PutPrivateData(collectionName, pvtDataKey, pvtDataBytes); err != nil {
+		return fmt.Errorf("could not put private data: %v", err)
+	}
+	return nil
+}
+
+func putStateWithCompositeKey[T any](stub shim.ChaincodeStubInterface, objectType string, keyAttributes []string, stateStruct T) error {
+	stateKey, err := stub.CreateCompositeKey(objectType, keyAttributes)
+	if err != nil {
+		return fmt.Errorf("could not create composite key for state: %v", err)
+	}
+	stateBytes, err := json.Marshal(stateStruct)
+	if err != nil {
+		return fmt.Errorf("could not marshal state: %v", err)
+	}
+	if err := stub.PutState(stateKey, stateBytes); err != nil {
+		return fmt.Errorf("could not put state: %v", err)
+	}
+	return nil
+}
