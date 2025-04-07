6f2804d48fc3b03884a837489c38a4cc5912668f
Johann Westphall <johannwestphall@gmail.com>
2025-04-07 02:09:18 -0300

    justify design to avoid MVCC READ CONFLICT

--- chaincodes/carbon/property.go
+++ chaincodes/carbon/property.go
@@ -7,7 +7,9 @@ type Coordinates struct {
 
 // PropertyChunk represents a chunk of a property.
 // It exists because properties might have heterogeneous chunks.
-// It points to the property because otherwise---
+// It points to the property because otherwise---if in a slice in the
+// property struct---it could generate MVCC_READ_CONFLICT errors.
+// See: https://github.com/hyperledger/fabric/issues/3748
 type PropertyChunk struct {
 	PropertyID  uint64
 	Coordinates []Coordinates
