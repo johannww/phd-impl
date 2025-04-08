e9ae2179cc1636fa1c7ba3a67ce5dc8d778467f8
Johann Westphall <johannwestphall@gmail.com>
2025-04-08 15:38:50 -0300

    add credit world state prefix

--- chaincodes/carbon/credits/credit.go
+++ chaincodes/carbon/credits/credit.go
@@ -2,6 +2,10 @@ package carbon
 
 import "github.com/hyperledger/fabric-chaincode-go/shim"
 
+const (
+	CREDIT_PREFIX = "credit"
+)
+
 // Credit represents a carbon unit minted for a property chunk
 // at a specific time.
 // TODO: enhance this struct
