eb040addf60286f2c3c0e17056c9909adb064a70
Johann Westphall <johannwestphall@gmail.com>
2025-04-08 14:38:31 -0300

    add credit mock world state implementation

--- chaincodes/carbon/credit.go
+++ chaincodes/carbon/credit.go
@@ -1,5 +1,7 @@
 package carbon
 
+import "github.com/hyperledger/fabric-chaincode-go/shim"
+
 // Credit represents a carbon unit minted for a property chunk
 // at a specific time.
 // TODO: enhance this struct
@@ -8,4 +10,16 @@ type Credit struct {
 	Owner    string        `json:"owner"`
 	Property Property      `json:"property"`
 	Chunk    PropertyChunk `json:"chunk"`
+}
+
+func (c *Credit) FromWorldState(stub shim.ChaincodeStubInterface, keyAttributes []string) (_ error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (c *Credit) ToWorldState(stub shim.ChaincodeStubInterface) (_ error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (c *Credit) GetID() (_ []string) {
+	panic("not implemented") // TODO: Implement
 }
