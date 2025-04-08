2b61e2d285f0640c5c896dfccc95820f37ed038d
Johann Westphall <johannwestphall@gmail.com>
2025-04-08 18:44:56 -0300

    add chaincode initial file

---
+++ chaincodes/carbon/carbon.go
@@ -0,0 +1,22 @@
+package carbon
+
+import (
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	pb "github.com/hyperledger/fabric-protos-go/peer"
+)
+
+type Carbon struct{}
+
+// Init is called during Instantiate transaction after the chaincode container
+// has been established for the first time, allowing the chaincode to
+// initialize its internal data
+func (carbon *Carbon) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	panic("not implemented") // TODO: Implement
+}
+
+// Invoke is called to update or query the ledger in a proposal transaction.
+// Updated state variables are not committed to the ledger until the
+// transaction is committed.
+func (carbon *Carbon) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	panic("not implemented") // TODO: Implement
+}
