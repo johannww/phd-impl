a3b7d09e80a4951dd6ea50c76feea392e13ef1c6
Johann Westphall <johannwestphall@gmail.com>
2025-04-10 19:29:53 -0300

    setup carbon chaincode name

--- chaincodes/carbon/cmd/carbon/main.go
+++ chaincodes/carbon/cmd/carbon/main.go
@@ -1,1 +1,34 @@
-package carbon
+package main
+
+import (
+	"os"
+
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	cc "github.com/johannww/phd-impl/chaincodes/carbon"
+)
+
+func main() {
+	ccId := os.Getenv("CHAINCODE_ID")
+	addr := os.Getenv("CHAINCODE_SERVER_ADDRESS")
+
+	if addr == "" {
+		if err := shim.Start(&cc.Carbon{}); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	server := shim.ChaincodeServer{
+		CCID:    ccId,
+		Address: addr,
+		CC:      &cc.Carbon{},
+		TLSProps: shim.TLSProperties{
+			Disabled: true,
+		},
+		KaOpts: nil,
+	}
+
+	if err := server.Start(); err != nil {
+		panic(err)
+	}
+}
