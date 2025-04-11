86f9e143e642254becdabee1e53fdb277da45103
Johann Westphall <johannwestphall@gmail.com>
2025-04-11 15:28:56 -0300

    add initial privatemultiplier

---
+++ chaincodes/carbon/bids/private_multiplier.go
@@ -0,0 +1,26 @@
+package bids
+
+import (
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	// ccstate "github.com/johannww/phd-impl/chaincodes/carbon/state"
+)
+
+const (
+	PVT_MULTIPLIER_PREFIX = "privateMultiplier"
+)
+
+// TODO: this may be a float64 passed to the chaincode via transient data
+// PrivateMultiplier is an for-the-government-only price encoded as a base64 string.
+type PrivateMultiplier struct{}
+
+func (privPrice *PrivateMultiplier) FromWorldState(stub shim.ChaincodeStubInterface, keyAttributes []string, extraPrefix string) error {
+	panic("not implemented")
+}
+
+func (privPrice *PrivateMultiplier) ToWorldState(stub shim.ChaincodeStubInterface, extraPrefix string) error {
+	panic("not implemented")
+}
+
+func (privPrice *PrivateMultiplier) GetID() []string {
+	panic("not implemented")
+}
