31a63c7b38b77d7c2c81d80b392b7baaf185bf13
Johann Westphall <johannwestphall@gmail.com>
2025-04-11 17:54:13 -0300

    add initial virtual token

--- chaincodes/carbon/payment/virtual_token.go
+++ chaincodes/carbon/payment/virtual_token.go
@@ -1,1 +1,44 @@
 package payment
+
+import "github.com/hyperledger/fabric-chaincode-go/shim"
+
+const (
+	PVT_DATA_COLLECTION      = "privateDataCollection"
+	PVT_VIRTUAL_TOKEn_PREFIX = "virtualToken"
+)
+
+type VirtualTokenWallet struct {
+	OwnerID  string  `json:"owner"`
+	Quantity float64 `json:"quantity"`
+}
+
+func MintVirtualToken(stub shim.ChaincodeStubInterface, ownerID string, quantity float64) (*VirtualTokenWallet, error) {
+	tokenWallet := &VirtualTokenWallet{OwnerID: ownerID}
+	err := tokenWallet.FromWorldState(stub, []string{ownerID})
+	// tokenWallet.FromWorldState
+	if err != nil {
+		return &VirtualTokenWallet{
+			OwnerID:  ownerID,
+			Quantity: quantity,
+		}, nil
+	}
+
+	tokenWallet.Quantity += quantity
+	err = tokenWallet.ToWorldState(stub)
+	if err != nil {
+		return nil, err
+	}
+	return tokenWallet, nil
+}
+
+func (virtualtokenwallet *VirtualTokenWallet) FromWorldState(stub shim.ChaincodeStubInterface, keyAttributes []string) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (virtualtokenwallet *VirtualTokenWallet) ToWorldState(stub shim.ChaincodeStubInterface) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (virtualtokenwallet *VirtualTokenWallet) GetID() []string {
+	panic("not implemented") // TODO: Implement
+}
