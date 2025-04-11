69f81858af8b752c281a4ea80ab2a39d0ff8a83a
Johann Westphall <johannwestphall@gmail.com>
2025-04-11 15:29:34 -0300

    add initial matched bid struct

--- chaincodes/carbon/bids/matchedbid.go
+++ chaincodes/carbon/bids/matchedbid.go
@@ -3,10 +3,11 @@ package bids
 // TODO: Perhaps I should keep separate structs.
 // One for the LevelDB and one for the application.
 type MatchedBid struct {
-	BuyBidID     []string      `json:"buyBidID"`
-	BuyBid       *BuyBid       `json:"buyBid"`
-	SellBidID    []string      `json:"sellBidID"`
-	SellBid      *SellBid      `json:"sellBid"`
-	Quantity     float64       `json:"quantity"`
-	PrivatePrice *PrivatePrice `json:"privatePrice"`
+	BuyBidID          []string           `json:"buyBidID"`
+	BuyBid            *BuyBid            `json:"buyBid"`
+	SellBidID         []string           `json:"sellBidID"`
+	SellBid           *SellBid           `json:"sellBid"`
+	Quantity          float64            `json:"quantity"`
+	PrivatePrice      *PrivatePrice      `json:"privatePrice"`
+	PrivateMultiplier *PrivateMultiplier `json:"privateMultiplier"`
 }
