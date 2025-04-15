9441f154c6b36450137315e8a4e34184b05356a4
Johann Westphall <johannwestphall@gmail.com>
2025-04-15 17:27:18 -0300

    add possible tls config for chaincode

--- chaincodes/carbon/cmd/carbon/main.go
+++ chaincodes/carbon/cmd/carbon/main.go
@@ -7,9 +7,21 @@ import (
 	cc "github.com/johannww/phd-impl/chaincodes/carbon"
 )
 
+func readTlsCert(tlsCertPath string) []byte {
+	x509Cert, err := os.ReadFile(tlsCertPath)
+	if err != nil {
+		panic("Failed to read TLS certificate: " + err.Error())
+	}
+	return x509Cert
+}
+
 func main() {
 	ccId := os.Getenv("CHAINCODE_ID")
 	addr := os.Getenv("CHAINCODE_SERVER_ADDRESS")
+	tlsDisabled := os.Getenv("CHAINCODE_TLS_DISABLED")
+	tlsCertPath := os.Getenv("CHAINCODE_TLS_CERT")
+	tlsKeyPath := os.Getenv("CHAINCODE_TLS_KEY")
+	tlsClientCACertPath := os.Getenv("CHAINCODE_CLIENT_CA_CERT")
 
 	if addr == "" {
 		if err := shim.Start(&cc.Carbon{}); err != nil {
@@ -18,14 +30,27 @@ func main() {
 		return
 	}
 
+	tlsProps := shim.TLSProperties{
+		Disabled: true,
+	}
+
+	if tlsDisabled == "false" {
+		if tlsCertPath == "" || tlsKeyPath == "" || tlsClientCACertPath == "" {
+			panic("TLS is enabled but required paths are not set")
+		}
+		tlsProps.Disabled = false
+		tlsProps.Key = readTlsCert(tlsKeyPath)
+		tlsProps.Cert = readTlsCert(tlsCertPath)
+		tlsProps.ClientCACerts = readTlsCert(tlsClientCACertPath)
+
+	}
+
 	server := shim.ChaincodeServer{
-		CCID:    ccId,
-		Address: addr,
-		CC:      &cc.Carbon{},
-		TLSProps: shim.TLSProperties{
-			Disabled: true,
-		},
-		KaOpts: nil,
+		CCID:     ccId,
+		Address:  addr,
+		CC:       &cc.Carbon{},
+		TLSProps: tlsProps,
+		KaOpts:   nil,
 	}
 
 	if err := server.Start(); err != nil {
