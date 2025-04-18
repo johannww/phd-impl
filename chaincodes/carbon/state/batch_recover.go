59e0f2f030e3fed0d1785edd43ba86bc3fa9a438
Johann Westphall <johannwestphall@gmail.com>
2025-04-18 01:54:54 -0300

    add todo to perform range queries without phantom reads

--- chaincodes/carbon/state/batch_recover.go
+++ chaincodes/carbon/state/batch_recover.go
@@ -1,3 +1,23 @@
 package state
 
-// TODO: make batch state recoveries here
+// import (
+// 	"encoding/json"
+// 	"fmt"
+//
+// 	"github.com/hyperledger/fabric-chaincode-go/shim"
+// )
+
+// TODO: think this in a way to avoid phantom read conflict
+// func GetStateWithPrefix(stub shim.ChaincodeStubInterface, objectType string, keyAttributes []string) ([]byte, error) {
+// 	stateIterator, metadata, err := stub.GetStateByRange(
+// 		objectType,
+// 		keyAttributes,
+// 		, bookmark)
+// 	if err != nil {
+// 		return fmt.Errorf("could not get state: %v", err)
+// 	}
+//
+// 	metadata.Bookmark
+//
+// 	return nil, nil
+// }
