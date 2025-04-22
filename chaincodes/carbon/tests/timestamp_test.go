81f6ddb312641631ccc88c9ee6b03fe3c5d42b53
Johann Westphall <johannwestphall@gmail.com>
2025-04-22 19:22:27 -0300

    add utc timestamp test

---
+++ chaincodes/carbon/tests/timestamp_test.go
@@ -0,0 +1,27 @@
+package carbon_tests
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/hyperledger/fabric-chaincode-go/shimtest"
+	"github.com/johannww/phd-impl/chaincodes/carbon"
+	"github.com/johannww/phd-impl/chaincodes/carbon/utils"
+)
+
+func TestUTCTimestamp(t *testing.T) {
+	stub := shimtest.NewMockStub("carbon", &carbon.Carbon{})
+	stub.MockTransactionStart("tx1")
+	ts, err := stub.GetTxTimestamp()
+	if err != nil {
+		t.Fatalf("Error getting timestamp: %v", err)
+	}
+
+	tsStr := utils.TimestampRFC3339UtcString(ts)
+
+	rfc3339Regex := `^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$`
+	matched, err := regexp.MatchString(rfc3339Regex, tsStr)
+	if err != nil || !matched {
+		t.Fatalf("tsStr is not a valid RFC3339 string: %s", tsStr)
+	}
+}
