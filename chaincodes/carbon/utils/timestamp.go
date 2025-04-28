039c20f02a99b31cfcb1f42ceba3eb5a8578501e
Johann Westphall <johannwestphall@gmail.com>
2025-04-28 14:05:28 -0300

    generalize timestamp. not exclusive to bids

--- chaincodes/carbon/utils/timestamp.go
+++ chaincodes/carbon/utils/timestamp.go
@@ -6,12 +6,12 @@ import (
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func TimestampRFC3339UtcString(bidTS *timestamppb.Timestamp) string {
-	if bidTS == nil {
+func TimestampRFC3339UtcString(ts *timestamppb.Timestamp) string {
+	if ts == nil {
 		return ""
 	}
 	// RFC3339     = "2006-01-02T15:04:05Z07:00"
 	// "AsTime" returns UTC time.
 	// We also use environment variable TZ=UTC to make sure
-	return bidTS.AsTime().Format(time.RFC3339)
+	return ts.AsTime().Format(time.RFC3339)
 }
