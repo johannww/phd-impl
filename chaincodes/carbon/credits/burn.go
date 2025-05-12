84a4298b184d0fc04bf653f23f8e7648a2d262fc
Johann Westphall <johannwestphall@gmail.com>
2025-05-12 18:17:38 -0300

    restructure mint/burn credits and add the state related methods

--- chaincodes/carbon/credits/burn.go
+++ chaincodes/carbon/credits/burn.go
@@ -1,9 +1,21 @@
 package credits
 
+import "github.com/hyperledger/fabric-chaincode-go/shim"
+
 // BurnCredit represents a minted carbon credit to be burned.
 // it is associated to burn multiplier and burn timestamp.
 type BurnCredit struct {
-	MintCredit    `json:"mintCredit"`
-	BurnMult      float64 `json:"burnMult"`
-	BurnTimeStamp uint64  `json:"burnTimestamp"`
+	MintCredit    MintCredit `json:"mintCredit"`
+	BurnMult      float64    `json:"burnMult"`
+	BurnTimeStamp uint64     `json:"burnTimestamp"`
+}
+
+func (bc *BurnCredit) FromWorldState(stub shim.ChaincodeStubInterface, keyAttributes []string) error {
+	panic("not implemented") // TODO: Implement
+}
+func (bc *BurnCredit) ToWorldState(stub shim.ChaincodeStubInterface) error {
+	panic("not implemented") // TODO: Implement
+}
+func (bc *BurnCredit) GetID() *[][]string {
+	return bc.MintCredit.GetID()
 }
--- chaincodes/carbon/credits/mint.go
+++ chaincodes/carbon/credits/mint.go
@@ -1,9 +1,21 @@
 package credits
 
+import "github.com/hyperledger/fabric-chaincode-go/shim"
+
 // MintCredit represents a carbon credit that has been minted and
 // it is associated to mint multiplier and mint timestamp.
 type MintCredit struct {
-	Credit        `json:"credit"`
+	Credit        Credit  `json:"credit"`
 	MintMult      float64 `json:"mintMult"`
 	MintTimeStamp uint64  `json:"mintTimestamp"`
+}
+
+func (mc *MintCredit) FromWorldState(stub shim.ChaincodeStubInterface, keyAttributes []string) error {
+	panic("not implemented") // TODO: Implement
+}
+func (mc *MintCredit) ToWorldState(stub shim.ChaincodeStubInterface) error {
+	panic("not implemented") // TODO: Implement
+}
+func (mc *MintCredit) GetID() *[][]string {
+	return mc.Credit.GetID()
 }
