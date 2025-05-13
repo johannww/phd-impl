c64c0d8c1ba6e7bcd66b6b745c200407393616e1
Johann Westphall <johannwestphall@gmail.com>
2025-05-13 16:39:12 -0300

    refactor batch recover

--- chaincodes/carbon/state/batch_recover.go
+++ chaincodes/carbon/state/batch_recover.go
@@ -18,6 +18,144 @@ func GetStatesByRange(stub shim.ChaincodeStubInterface, objectType string, key s
 	return nil, nil
 }
 
+func GetStatesByRangeCompositeKey[T any](stub shim.ChaincodeStubInterface, objectType string, startPrefixes, endPrefixes []string) ([]*T, error) {
+	endKey, startKey, err := getRangeCompositeKeys(stub, objectType, startPrefixes, endPrefixes)
+	if err != nil {
+		return nil, fmt.Errorf("could not create composite key for: %v", err)
+	}
+
+	states, err := getStatesByRange[T](stub, startKey, endKey)
+	return states, nil
+}
+
+func GetStatesBytesByRangeCompositeKey(stub shim.ChaincodeStubInterface, objectType string, startPrefixes, endPrefixes []string) ([][]byte, error) {
+	endKey, startKey, err := getRangeCompositeKeys(stub, objectType, startPrefixes, endPrefixes)
+	if err != nil {
+		return nil, fmt.Errorf("could not create composite key for: %v", err)
+	}
+
+	states, err := getStatesBytesByRange(stub, startKey, endKey)
+	return states, err
+}
+
+// NOTE: This is currently not used. It is kept because I have implemented it
+func GetStatesByRangeCompositeKeyReadOnly(stub shim.ChaincodeStubInterface, objectType string, startPrefixes, endPrefixes []string) ([][]byte, error) {
+	endKey, startKey, err := getRangeCompositeKeys(stub, objectType, startPrefixes, endPrefixes)
+	if err != nil {
+		return nil, fmt.Errorf("could not create composite key for: %v", err)
+	}
+
+	bookmark := ""
+	states := [][]byte{}
+	for {
+		stateIterator, metadata, err := stub.GetStateByRangeWithPagination(
+			startKey,
+			endKey,
+			PAGE_SIZE, bookmark)
+		if err != nil {
+			return nil, fmt.Errorf("could not get state by range for start key %s and end key %s: %v", startKey, endKey, err)
+		}
+		defer stateIterator.Close()
+
+		if metadata.FetchedRecordsCount == 0 {
+			break
+		}
+
+		statesInRange, err := readIteratorStatesPagination(stateIterator, metadata)
+		if err != nil {
+			return nil, fmt.Errorf("could not read iterator states: %v", err)
+		}
+		states = append(states, statesInRange...)
+
+		bookmark = metadata.Bookmark
+		if bookmark == "" {
+			break
+		}
+
+	}
+
+	return states, nil
+}
+
+// TODO: TEST THIS
+func GetStatesBytesByPartialCompositeKey(stub shim.ChaincodeStubInterface, objectType string, prefixes []string) ([][]byte, error) {
+	startKey, err := stub.CreateCompositeKey(objectType, prefixes)
+	endKey := startKey + string(maxUnicodeRuneValue)
+	if err != nil {
+		return nil, fmt.Errorf("could not create composite key for: %v", err)
+	}
+
+	states, err := getStatesBytesByRange(stub, startKey, endKey)
+	return states, err
+}
+
+func GetStatesByPartialCompositeKey[T any](stub shim.ChaincodeStubInterface, objectType string, prefixes []string) ([]*T, error) {
+	startKey, err := stub.CreateCompositeKey(objectType, prefixes)
+	endKey := startKey + string(maxUnicodeRuneValue)
+	if err != nil {
+		return nil, fmt.Errorf("could not create composite key for: %v", err)
+	}
+
+	states, err := getStatesByRange[T](stub, startKey, endKey)
+	return states, err
+}
+
+func getStatesByRange[T any](stub shim.ChaincodeStubInterface, startKey, endKey string) ([]*T, error) {
+	states := []*T{}
+
+	for {
+		stateIterator, err := stub.GetStateByRange(startKey, endKey)
+		if err != nil {
+			return nil, fmt.Errorf("could not get state by range for start key %s and end key %s: %v", startKey, endKey, err)
+		}
+		defer stateIterator.Close()
+
+		statesInRange, lastKey, err := readUnmarshalledIteratorStates[T](stateIterator)
+		if err != nil {
+			return nil, fmt.Errorf("could not read iterator states: %v", err)
+		}
+		states = append(states, statesInRange...)
+
+		// If lastKey is not empty, we should read again.
+		// GetStateByRange is capped by "totalQueryLimit"
+		if lastKey != "" {
+			startKey = lastKey + string(maxUnicodeRuneValue)
+		} else {
+			break
+		}
+	}
+
+	return states, nil
+}
+
+func getStatesBytesByRange(stub shim.ChaincodeStubInterface, startKey, endKey string) ([][]byte, error) {
+	states := [][]byte{}
+
+	for {
+		stateIterator, err := stub.GetStateByRange(startKey, endKey)
+		if err != nil {
+			return nil, fmt.Errorf("could not get state by range for start key %s and end key %s: %v", startKey, endKey, err)
+		}
+		defer stateIterator.Close()
+
+		statesInRange, lastKey, err := readIteratorStates(stateIterator)
+		if err != nil {
+			return nil, fmt.Errorf("could not read iterator states: %v", err)
+		}
+		states = append(states, statesInRange...)
+
+		// If lastKey is not empty, we should read again.
+		// GetStateByRange is capped by "totalQueryLimit"
+		if lastKey != "" {
+			startKey = lastKey + string(maxUnicodeRuneValue)
+		} else {
+			break
+		}
+	}
+
+	return states, nil
+}
+
 func readIteratorStates(stateIterator shim.StateQueryIteratorInterface) ([][]byte, string, error) {
 	statesInRange := [][]byte{}
 	i := 0
@@ -95,121 +233,4 @@ func getRangeCompositeKeys(stub shim.ChaincodeStubInterface, objectType string, 
 		return "", "", fmt.Errorf("could not create composite key for: %v", err)
 	}
 	return endKey, startKey, nil
-}
-
-func GetStatesByRangeCompositeKey[T any](stub shim.ChaincodeStubInterface, objectType string, startPrefixes, endPrefixes []string) ([]*T, error) {
-	endKey, startKey, err := getRangeCompositeKeys(stub, objectType, startPrefixes, endPrefixes)
-	if err != nil {
-		return nil, fmt.Errorf("could not create composite key for: %v", err)
-	}
-
-	states := []*T{}
-	for {
-		stateIterator, err := stub.GetStateByRange(startKey, endKey)
-		if err != nil {
-			return nil, fmt.Errorf("could not get state by range for start key %s and end key %s: %v", startKey, endKey, err)
-		}
-		defer stateIterator.Close()
-
-		statesInRange, lastKey, err := readUnmarshalledIteratorStates[T](stateIterator)
-		if err != nil {
-			return nil, fmt.Errorf("could not read iterator states: %v", err)
-		}
-		states = append(states, statesInRange...)
-
-		// If lastKey is not empty, we should read again.
-		// GetStateByRange is capped by "totalQueryLimit"
-		if lastKey != "" {
-			startKey = lastKey + string(maxUnicodeRuneValue)
-		} else {
-			break
-		}
-
-	}
-	return states, nil
-}
-
-func GetStatesBytesByRangeCompositeKey(stub shim.ChaincodeStubInterface, objectType string, startPrefixes, endPrefixes []string) ([][]byte, error) {
-	endKey, startKey, err := getRangeCompositeKeys(stub, objectType, startPrefixes, endPrefixes)
-	if err != nil {
-		return nil, fmt.Errorf("could not create composite key for: %v", err)
-	}
-
-	states := [][]byte{}
-	for {
-		stateIterator, err := stub.GetStateByRange(startKey, endKey)
-		if err != nil {
-			return nil, fmt.Errorf("could not get state by range for start key %s and end key %s: %v", startKey, endKey, err)
-		}
-		defer stateIterator.Close()
-
-		statesInRange, lastKey, err := readIteratorStates(stateIterator)
-		if err != nil {
-			return nil, fmt.Errorf("could not read iterator states: %v", err)
-		}
-		states = append(states, statesInRange...)
-
-		// If lastKey is not empty, we should read again.
-		// GetStateByRange is capped by "totalQueryLimit"
-		if lastKey != "" {
-			startKey = lastKey + string(maxUnicodeRuneValue)
-		} else {
-			break
-		}
-
-	}
-	return states, nil
-}
-
-// NOTE: This is currently not used. It is kept because I have implemented it
-func GetStatesByRangeCompositeKeyReadOnly(stub shim.ChaincodeStubInterface, objectType string, startPrefixes, endPrefixes []string) ([][]byte, error) {
-	endKey, startKey, err := getRangeCompositeKeys(stub, objectType, startPrefixes, endPrefixes)
-	if err != nil {
-		return nil, fmt.Errorf("could not create composite key for: %v", err)
-	}
-
-	bookmark := ""
-	states := [][]byte{}
-	for {
-		stateIterator, metadata, err := stub.GetStateByRangeWithPagination(
-			startKey,
-			endKey,
-			PAGE_SIZE, bookmark)
-		if err != nil {
-			return nil, fmt.Errorf("could not get state by range for start key %s and end key %s: %v", startKey, endKey, err)
-		}
-		defer stateIterator.Close()
-
-		if metadata.FetchedRecordsCount == 0 {
-			break
-		}
-
-		statesInRange, err := readIteratorStatesPagination(stateIterator, metadata)
-		if err != nil {
-			return nil, fmt.Errorf("could not read iterator states: %v", err)
-		}
-		states = append(states, statesInRange...)
-
-		bookmark = metadata.Bookmark
-		if bookmark == "" {
-			break
-		}
-
-	}
-
-	return states, nil
-}
-
-// TODO: TEST THIS
-func GetStatesBytesByPartialCompositeKey(stub shim.ChaincodeStubInterface, objectType string, prefixes []string) ([][]byte, error) {
-	startPrefixes := prefixes
-	endPrefixes := append(prefixes, string(maxUnicodeRuneValue))
-	return GetStatesBytesByRangeCompositeKey(stub, objectType, startPrefixes, endPrefixes)
-}
-
-// TODO: TEST THIS
-func GetStatesByPartialCompositeKey[T any](stub shim.ChaincodeStubInterface, objectType string, prefixes []string) ([]*T, error) {
-	startPrefixes := prefixes
-	endPrefixes := append(prefixes, string(maxUnicodeRuneValue))
-	return GetStatesByRangeCompositeKey[T](stub, objectType, startPrefixes, endPrefixes)
 }
