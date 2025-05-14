c24f505b37b39f9c7210a305e1c1bccdd6d4ef74
Johann Westphall <johannwestphall@gmail.com>
2025-05-14 16:50:34 -0300

    feat: credit wallet for fungible credits

---
+++ chaincodes/carbon/credits/wallet.go
@@ -0,0 +1,37 @@
+package credits
+
+import (
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	"github.com/johannww/phd-impl/chaincodes/carbon/state"
+)
+
+const (
+	CREDIT_WALLET_PREFIX = "creditWallet"
+)
+
+// CreditWallet is for networks with fungible credits
+type CreditWallet struct {
+	OwnerID  string  `json:"owner"`
+	Quantity float64 `json:"quantity"`
+}
+
+var _ state.WorldStateManager = (*CreditWallet)(nil)
+
+func (cw *CreditWallet) FromWorldState(stub shim.ChaincodeStubInterface, keyAttributes []string) error {
+	// NOTE: We are using the same private collection as the one used for the private price
+	err := state.GetPvtDataWithCompositeKey(stub, CREDIT_WALLET_PREFIX, keyAttributes, state.BIDS_PVT_DATA_COLLECTION, cw)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (cw *CreditWallet) ToWorldState(stub shim.ChaincodeStubInterface) error {
+	// NOTE: We are using the same private collection as the one used for the private price
+	return state.PutPvtDataWithCompositeKey(
+		stub, CREDIT_WALLET_PREFIX,
+		(*cw.GetID())[0],
+		state.BIDS_PVT_DATA_COLLECTION, cw)
+}
+func (cw *CreditWallet) GetID() *[][]string {
+	return &[][]string{{cw.OwnerID}}
+}
