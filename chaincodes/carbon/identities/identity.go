9feb46c61944aa3a7b04593fa6f847260aaf1573
Johann Westphall <johannwestphall@gmail.com>
2025-05-14 17:57:43 -0300

    create GetID function for string representations of x509 and Idemix

--- chaincodes/carbon/identities/identity.go
+++ chaincodes/carbon/identities/identity.go
@@ -1,5 +1,14 @@
 package identities
 
+import (
+	"fmt"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/hyperledger/fabric-chaincode-go/pkg/cid"
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	"github.com/hyperledger/fabric-protos-go/msp"
+)
+
 // TODO: add attributes
 type Identity interface {
 	String() string
@@ -13,4 +22,28 @@ func (x509identity *X509Identity) String() string {
 	return x509identity.CertID
 }
 
-// TODO: read about idemix identities
+func GetID(stub shim.ChaincodeStubInterface) string {
+	id, err := cid.GetID(stub)
+	if err != nil {
+		// idemix identity
+		creator, _ := stub.GetCreator()
+		if creator == nil {
+			panic("idemix identity creator is nil")
+		}
+
+		sid := &msp.SerializedIdentity{}
+		err := proto.Unmarshal(creator, sid)
+		if err != nil {
+			panic(fmt.Sprintf("failed to unmarshal SerializedIdentity: %s", err))
+		}
+		idemixID := &msp.SerializedIdemixIdentity{}
+		err = proto.Unmarshal(sid.IdBytes, idemixID)
+		if err != nil {
+			panic(fmt.Sprintf("failed to unmarshal SerializedIdemixIdentity: %s", err))
+		}
+
+		return fmt.Sprintf("%x%x", idemixID.NymX[0:8], idemixID.NymY[8:16])
+	}
+
+	return id
+}
