ff2b22ab36b3d2dadf1583fa1a4ff88201b75bfe
Johann Westphall <johannwestphall@gmail.com>
2025-05-15 13:57:28 -0300

    feat: save testdata on world state

--- chaincodes/carbon/tests/utils/testdata.go
+++ chaincodes/carbon/tests/utils/testdata.go
@@ -1,6 +1,7 @@
 package utils_test
 
 import (
+	"github.com/hyperledger/fabric-chaincode-go/shim"
 	"github.com/johannww/phd-impl/chaincodes/carbon/properties"
 	setup "github.com/johannww/phd-impl/chaincodes/carbon/tests/setup"
 )
@@ -10,4 +11,12 @@ import (
 type TestData struct {
 	Identities *setup.MockIdentities
 	Properties []*properties.Property
+}
+
+func (data *TestData) SaveToWorldState(stub shim.ChaincodeStubInterface) {
+	for _, prop := range data.Properties {
+		if err := prop.ToWorldState(stub); err != nil {
+			panic(err)
+		}
+	}
 }
