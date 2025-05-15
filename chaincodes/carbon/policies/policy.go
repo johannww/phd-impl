66efa631da3ccfbb9152757f1d517e0fd39d639b
Johann Westphall <johannwestphall@gmail.com>
2025-05-15 16:31:22 -0300

    add initial policy calculation function

--- chaincodes/carbon/policies/policy.go
+++ chaincodes/carbon/policies/policy.go
@@ -1,7 +1,14 @@
 package policies
 
+import "github.com/johannww/phd-impl/chaincodes/carbon/properties"
+
 type IndependentPolicy interface {
 }
 
 type CoupledPolicy interface {
+}
+
+// TODO: implement this
+func MintIndependentMult(chunk *properties.PropertyChunk) float64 {
+	return 1.0
 }
