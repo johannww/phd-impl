7f17b67b406d5bbbad8526ed4c7f8a8741b363ce
Johann Westphall <johannwestphall@gmail.com>
2025-05-25 00:09:39 -0300

    set org1 tls cert and org3 idemix msp

--- chaincodes/carbon/tests/scripts/test-idemix-app-invoke.sh
+++ chaincodes/carbon/tests/scripts/test-idemix-app-invoke.sh
@@ -12,9 +12,9 @@ export PATH=$PATH:$(realpath ../bin)
 export FABRIC_CFG_PATH=$(realpath ../config)
 
 export CORE_PEER_TLS_ENABLED=true
-export CORE_PEER_LOCALMSPID=Org1MSP
-export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/tlsca/tlsca.org2.example.com-cert.pem
-export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/User2@org2.example.com/msp
+export CORE_PEER_LOCALMSPID=Org3MSP
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org3.example.com/users/User1@org3.example.com/msp
 export CORE_PEER_ADDRESS=localhost:7051
 export PATH=${PWD}/../bin:$PATH
 export FABRIC_CFG_PATH=${PWD}/../config
@@ -27,4 +27,4 @@ cd ..
 APP_CMD_MAIN=./cmd/application/main.go
 tree $CORE_PEER_MSPCONFIGPATH
 # TODO: this is not working yet
-go run $APP_CMD_MAIN --idemix true --mspId Org2MSP --mspPath $CORE_PEER_MSPCONFIGPATH
+go run $APP_CMD_MAIN --idemix true --mspId Org3MSP --mspPath $CORE_PEER_MSPCONFIGPATH
