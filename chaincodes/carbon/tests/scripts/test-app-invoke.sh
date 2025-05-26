e8cc8f26841fc777fc1e18f15387ded3443acb3b
Johann Westphall <johannwestphall@gmail.com>
2025-05-26 01:20:19 -0300

    add test app invoke with x509 certificate. It works.

---
+++ chaincodes/carbon/tests/scripts/test-app-invoke.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Environment variables for Org1
+
+SCRIPT_DIR=$(dirname "$0")
+cd $SCRIPT_DIR/..
+FABRIC_SAMPLES_DIR=fabric-samples
+TEST_NETWORK_DIR=fabric-samples/test-network
+
+pushd $TEST_NETWORK_DIR
+export PATH=$PATH:$(realpath ../bin)
+export FABRIC_CFG_PATH=$(realpath ../config)
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID=Org1MSP
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=${PWD}/../config
+export ORDERER_TLS_ROOTCERT_FILE=${PWD}/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
+
+popd
+
+cd ..
+
+# invoke the function
+# peer chaincode query -C mychannel -n carbon -c '{"Args":["org.hyperledger.fabric:GetMetadata"]}'
+APP_CMD_MAIN=./cmd/application/main.go
+tree $CORE_PEER_MSPCONFIGPATH
+go run $APP_CMD_MAIN --mspId Org1MSP --mspPath $CORE_PEER_MSPCONFIGPATH
