bb5820702e8b84c7fe2c61109706440056e57db5
Johann Westphall <johannwestphall@gmail.com>
2025-05-30 23:24:52 -0300

    improve todo description

--- tee_auction/go/cmd/auction/main.go
+++ tee_auction/go/cmd/auction/main.go
@@ -9,6 +9,7 @@ func main() {
 	// Put the public key's hash in the AMD SEV-SNP report
 	// on ReportData field. See:
 	// https://github.com/johannww/ubuntu-learning/blob/4933313b537c6283dbef8eb093b3924611c5061c/crypto/azure_tee/containers/report_attester/snp_attestation_report.go#L39-L40
+	// See page 56 (REPORT_DATA) of https://www.amd.com/content/dam/amd/en/documents/epyc-technical-docs/specifications/56860.pdf
 
 	// Wait for requests
 
