b78fb3b7be81c752c075c419cd93e1f7662a407d
Johann Westphall <johannwestphall@gmail.com>
2025-05-30 23:10:54 -0300

    complement todos

--- tee_auction/go/cmd/auction/main.go
+++ tee_auction/go/cmd/auction/main.go
@@ -4,4 +4,12 @@ package main
 // Azure Confidential Container.
 func main() {
 
+	// Generate key pair
+
+	// Put the public key's hash in the AMD SEV-SNP report
+	// on ReportData field. See:
+	// https://github.com/johannww/ubuntu-learning/blob/4933313b537c6283dbef8eb093b3924611c5061c/crypto/azure_tee/containers/report_attester/snp_attestation_report.go#L39-L40
+
+	// Wait for requests
+
 }
