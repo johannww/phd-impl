32727512402994473445692f178de13053843681
Johann Westphall <johannwestphall@gmail.com>
2025-06-26 14:10:53 -0300

    add forest density as a param of a vegetation prop

--- chaincodes/carbon/vegetation/vegetation.go
+++ chaincodes/carbon/vegetation/vegetation.go
@@ -1,6 +1,7 @@
 package vegetation
 
 type VegetationProps struct {
-	ForestType ForestType `json:"forestType,omitempty"`
-	CropType   CropType   `json:"cropType,omitempty"`
+	ForestType    ForestType `json:"forestType,omitempty"`
+	ForestDensity float64    `json:"forestDensity,omitempty"`
+	CropType      CropType   `json:"cropType,omitempty"`
 }
