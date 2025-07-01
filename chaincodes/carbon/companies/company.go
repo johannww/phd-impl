631592225a260d1cdffd2ea9ef1a96e90417bee7
Johann Westphall <johannwestphall@gmail.com>
2025-07-01 16:34:17 -0300

    add company initial implementation

--- chaincodes/carbon/companies/company.go
+++ chaincodes/carbon/companies/company.go
@@ -1,12 +1,35 @@
 package companies
 
 import (
+	"github.com/hyperledger/fabric-chaincode-go/v2/shim"
 	"github.com/johannww/phd-impl/chaincodes/carbon/data"
 	"github.com/johannww/phd-impl/chaincodes/carbon/properties"
+	"github.com/johannww/phd-impl/chaincodes/carbon/state"
 )
 
-// TODO: continue implementing
+const COMPANY_PREFIX = "company"
+
+// Company represent a company stored as private data in the world state.
 type Company struct {
-	Coordinate properties.Coordinate
-	DataProps  []*data.ValidationProps
+	ID         string                  // ID might be the CNPJ (brazilian company national ID)
+	Coordinate properties.Coordinate   // Geographical coordinate in floating point format
+	DataProps  []*data.ValidationProps // How data from the company is validated
+}
+
+var _ state.WorldStateManager = (*Company)(nil)
+
+// FromWorldState implements state.WorldStateManager.
+func (c *Company) FromWorldState(stub shim.ChaincodeStubInterface, keyAttributes []string) error {
+	return state.GetPvtDataWithCompositeKey(stub, COMPANY_PREFIX, keyAttributes, state.BIDS_PVT_DATA_COLLECTION, c)
+}
+
+// GetID implements state.WorldStateManager.
+func (c *Company) GetID() *[][]string {
+	return &[][]string{{c.ID}}
+}
+
+// ToWorldState implements state.WorldStateManager.
+func (c *Company) ToWorldState(stub shim.ChaincodeStubInterface) error {
+	firstID := (*c.GetID())[0]
+	return state.PutPvtDataWithCompositeKey(stub, COMPANY_PREFIX, firstID, state.BIDS_PVT_DATA_COLLECTION, c)
 }
