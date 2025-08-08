f2ef321836af03d292108e4b7be8894ff3ca4388
Johann Westphall <johannwestphall@gmail.com>
2025-08-08 11:14:17 -0300

    echo messages about diagram generations

--- chaincodes/carbon/docs/scripts/generate-diagrams.sh
+++ chaincodes/carbon/docs/scripts/generate-diagrams.sh
@@ -9,5 +9,9 @@ go-plantuml generate \
     -d "${dirs}" \
     -o ./docs/diagrams/diagram.puml
 
+echo "plantuml file generated ./docs/diagrams/diagram.puml"
+
 plantuml -d ./docs/diagrams/diagram.puml -tsvg
+
+echo "diagrams generated in ./docs/diagrams/diagram.svg"
 
