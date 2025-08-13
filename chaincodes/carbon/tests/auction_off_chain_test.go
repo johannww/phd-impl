9987b4cc3d020aa943ff17b31658b39e0f3b7881
Johann Westphall <johannwestphall@gmail.com>
2025-08-12 23:55:42 -0300

    add test with random prices

--- chaincodes/carbon/tests/auction_off_chain_test.go
+++ chaincodes/carbon/tests/auction_off_chain_test.go
@@ -1,10 +1,12 @@
 package carbon_tests
 
 import (
+	"math/rand"
 	"testing"
 	"time"
 
 	"github.com/johannww/phd-impl/chaincodes/carbon/auction"
+	"github.com/johannww/phd-impl/chaincodes/carbon/bids"
 	"github.com/johannww/phd-impl/chaincodes/carbon/identities"
 	mocks "github.com/johannww/phd-impl/chaincodes/carbon/state/mocks"
 	utils_test "github.com/johannww/phd-impl/chaincodes/carbon/tests/utils"
@@ -17,6 +19,34 @@ func TestOffChainIndependentAuction(t *testing.T) {
 	issueStart, err := time.Parse(time.RFC3339, "2023-01-01T00:31:00Z")
 	require.NoError(t, err)
 	issueEnd := genAllMatchedBids(testData, issueStart)
+
+	stub.MockTransactionStart("tx1")
+	testData.SaveToWorldState(stub)
+	stub.MockTransactionEnd("tx1")
+
+	setAuctionType(t, stub, "tx2")
+
+	auctionData := retriveAuctionDataFromWorldState(t, stub, issueEnd, testData, "tx3")
+
+	var totalBuyBidQuantity int64 = 0
+	for _, bid := range auctionData.BuyBids {
+		totalBuyBidQuantity += bid.AskQuantity
+	}
+
+	// Run the auction
+	auctionResult, err := auction.RunIndependent(auctionData)
+	require.NoError(t, err, "Failed to run independent auction")
+
+	verifyBidsQuantityConsistency(t, totalBuyBidQuantity, auctionResult)
+}
+
+// TODOHP: continue here. test with random bids for credits
+func TestOffChainIndependentAuctionWithRandomBids(t *testing.T) {
+	stub, testData := genTestDataAndStub()
+
+	issueStart, err := time.Parse(time.RFC3339, "2023-01-01T00:31:00Z")
+	require.NoError(t, err)
+	issueEnd := genRandomBidsForMintCredits(testData, issueStart)
 
 	stub.MockTransactionStart("tx1")
 	testData.SaveToWorldState(stub)
@@ -94,4 +124,47 @@ func retriveAuctionDataFromWorldState(
 	stub.MockTransactionEnd(txID)
 	require.NoError(t, err, "Failed to retrieve auction data")
 	return auctionData
+}
+
+func genRandomBidsForMintCredits(testData *utils_test.TestData, issueStart time.Time) (lastIssueRFC339Ts string) {
+	sellMinPrice := int64(1000)
+	buyMinPrice := int64(1000)
+
+	buyerIds := testData.PseudonymMap
+
+	var issueTsStr string
+	for i, mintCredit := range testData.MintCredits {
+		issueTs := issueStart.Add(time.Duration(time.Duration(i) * time.Second)).UTC()
+		issueTsStr = issueTs.Format(time.RFC3339)
+		sellPrice := sellMinPrice + int64(rand.Intn(1000)) // Randomize sell price
+		sellBid := &bids.SellBid{
+			SellerID:  mintCredit.OwnerID,
+			CreditID:  (*mintCredit.GetID())[0],
+			Timestamp: issueTsStr,
+			PrivatePrice: &bids.PrivatePrice{
+				Price: sellPrice,
+			},
+			Quantity: mintCredit.Quantity,
+		}
+		sellBid.PrivatePrice.BidID = (*sellBid.GetID())[0]
+
+		testData.SellBids = append(testData.SellBids, sellBid)
+
+		buyerIdIndex := rand.Intn(len(buyerIds))
+
+		buyPrice := buyMinPrice + int64(rand.Intn(1000)) // Randomize buy price
+		buyBid := &bids.BuyBid{
+			BuyerID:     buyerIds[buyerIdIndex].Pseudonym,
+			AskQuantity: mintCredit.Quantity,
+			Timestamp:   issueTsStr,
+			PrivatePrice: &bids.PrivatePrice{
+				Price: buyPrice,
+			},
+		}
+		buyBid.PrivatePrice.BidID = (*buyBid.GetID())[0]
+
+		testData.BuyBids = append(testData.BuyBids, buyBid)
+	}
+
+	return issueTsStr
 }
