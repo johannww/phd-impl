d64f8215a84dcf7c88c5f0694f33c4d089ce0005
Johann Westphall <johannwestphall@gmail.com>
2025-08-13 18:13:15 -0300

    add mock state object for testing purposes

---
+++ chaincodes/carbon/state/mocks/mock_world_state_object.go
@@ -0,0 +1,58 @@
+package mocks
+
+import (
+	"github.com/hyperledger/fabric-chaincode-go/v2/shim"
+	"github.com/johannww/phd-impl/chaincodes/carbon/state"
+)
+
+const (
+	MOCK_OBJECT_PREFIX = "mockObject"
+	MOCK_OBJECT_PVT    = "mockObjectPvt"
+)
+
+type MockObjectWithSecondaryIndex struct {
+	MockAttr string `json:"mockAttr"`
+	MockPvt  string `json:"mockPvtAttr"`
+}
+
+var _ state.WorldStateManager = (*MockObjectWithSecondaryIndex)(nil)
+
+// FromWorldState implements WorldStateManager.
+func (m *MockObjectWithSecondaryIndex) FromWorldState(stub shim.ChaincodeStubInterface, keyAttributes []string) error {
+	if err := state.GetStateWithCompositeKey(stub, MOCK_OBJECT_PREFIX, keyAttributes, m); err != nil {
+		return err
+	}
+
+	if err := state.GetPvtDataWithCompositeKey(stub, MOCK_OBJECT_PREFIX, keyAttributes, MOCK_OBJECT_PVT, m); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetID implements WorldStateManager.
+func (m *MockObjectWithSecondaryIndex) GetID() *[][]string {
+	return &[][]string{
+		{m.MockAttr, "1"},
+		{m.MockAttr, "2"},
+	}
+}
+
+// ToWorldState implements WorldStateManager.
+func (m *MockObjectWithSecondaryIndex) ToWorldState(stub shim.ChaincodeStubInterface) error {
+	copyMock := *m
+	copyMock.MockPvt = "" // Temporarily unset MockPvt to avoid storing it in the public world state
+	if err := state.PutStateWithCompositeKey(stub, MOCK_OBJECT_PREFIX, m.GetID(), &copyMock); err != nil {
+		return err
+	}
+
+	if m.MockPvt == "" {
+		return nil // No private data to store
+	}
+
+	if err := state.PutPvtDataWithCompositeKey(stub, MOCK_OBJECT_PREFIX, (*m.GetID())[0], MOCK_OBJECT_PVT, &m); err != nil {
+		return err
+	}
+
+	return nil
+}
