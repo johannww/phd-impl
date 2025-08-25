3210527c6d8887d5915c5b17e3e5dbd7e39f4eff
Johann Westphall <johannwestphall@gmail.com>
2025-08-25 17:51:13 -0300

    add example policies

--- chaincodes/carbon/policies/distance.go
+++ chaincodes/carbon/policies/distance.go
@@ -1,6 +1,7 @@
 package policies
 
 // TODO: Implement
+// DistancePolicy is a placeholder policy that always returns a fixed multiplier of 500 (i.e., 0.5x).
 func DistancePolicy(input *PolicyInput) int64 {
 	return 500
 }
--- chaincodes/carbon/policies/wind.go
+++ chaincodes/carbon/policies/wind.go
@@ -1,6 +1,7 @@
 package policies
 
 // TODO: Implement
+// WindPolicy is a placeholder policy that always returns a fixed multiplier of 500 (i.e., 0.5x).
 func WindPolicy(input *PolicyInput) int64 {
 	return 500
 }
