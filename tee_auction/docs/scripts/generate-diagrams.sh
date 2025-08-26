c525d2302e0c4e0e9961e801125a56af4ceb32c7
Johann Westphall <johannwestphall@gmail.com>
2025-08-26 17:59:06 -0300

    adapt go-plantuml output to add vendored folders

--- tee_auction/docs/scripts/generate-diagrams.sh
+++ tee_auction/docs/scripts/generate-diagrams.sh
@@ -2,13 +2,14 @@
 
 mkdir -p ./docs/diagrams
 
-dirs="go/auction,go/api,go/cmd/auction"
+dirs="go/auction,go/api,go/cmd/auction,go/vendor/github.com/johannww/phd-impl/chaincodes/carbon/auction"
 
 go-plantuml generate \
     -d "${dirs}" \
     -o ./docs/diagrams/diagram.puml
 
 echo "plantuml file generated ./docs/diagrams/diagram.puml"
+sed -i 's/github\.com/github-com/g' ./docs/diagrams/diagram.puml
 
 plantuml -d ./docs/diagrams/diagram.puml -tsvg
 
