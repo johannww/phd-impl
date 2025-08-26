fb928f4dd01a824194a1f20227922e3f525d944a
Johann Westphall <johannwestphall@gmail.com>
2025-08-26 17:18:34 -0300

    add docs generate-diagrams to tee_auction also

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 diagrams:
-	cd ./chaincodes/carbon/ && ./docs/scripts/generate-diagrams.sh
+	$(MAKE) -C ./chaincodes/carbon docs
+	$(MAKE) -C tee_auction docs
 
 test:
 	cd ./chaincodes/carbon/ && go test -count=1 ./tests
---
+++ tee_auction/docs/scripts/generate-diagrams.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+mkdir -p ./docs/diagrams
+
+dirs="go/auction"
+
+go-plantuml generate \
+    -d "${dirs}" \
+    -o ./docs/diagrams/diagram.puml
+
+echo "plantuml file generated ./docs/diagrams/diagram.puml"
+
+plantuml -d ./docs/diagrams/diagram.puml -tsvg
+
+echo "diagrams generated in ./docs/diagrams/diagram.svg"
+
