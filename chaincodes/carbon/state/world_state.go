74c80fef8ac28d7c94e5c1da04f12d2409d215d7
Johann Westphall <johannwestphall@gmail.com>
2025-08-27 12:15:13 -0300

    add documentation for WorldStateManagerWithExtraPrefix

--- chaincodes/carbon/state/world_state.go
+++ chaincodes/carbon/state/world_state.go
@@ -12,6 +12,10 @@ type WorldStateManager interface {
 	GetID() *[][]string
 }
 
+// WorldStateManagerWithExtraPrefix is used by structs saved as private fields of others
+// The extra prefix allows mapping between the type of the containing struct
+// and the contained struct (which implements WorldStateManagerWithExtraPrefix)
+// The relationship between BuyBid and PrivatePrice illustrate that use case.
 type WorldStateManagerWithExtraPrefix interface {
 	FromWorldState(stub shim.ChaincodeStubInterface, keyAttributes []string, extraPrefix string) error
 	ToWorldState(stub shim.ChaincodeStubInterface, extraPrefix string) error
