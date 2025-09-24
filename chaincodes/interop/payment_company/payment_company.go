43127c025c4656907e6ad7a8bcf7d0cb4c18666f
Johann Westphall <johannwestphall@gmail.com>
2025-09-24 16:56:08 -0300

    add international payment company struct

---
+++ chaincodes/interop/payment_company/payment_company.go
@@ -0,0 +1,9 @@
+package paymentcompany
+
+// PaymentCompany represents a company that confirm payments
+// in cross-chain transactions
+// TODO: implement the logic to manage payment companies
+type PaymentCompany struct {
+	Name string `json:"name"`
+	ID   string `json:"id"`
+}
