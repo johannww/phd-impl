91f12a53dd28a5742b505fa118147914474b455c
Johann Westphall <johannwestphall@gmail.com>
2026-03-06 04:24:02 -0300

    assert that attribute is present

--- experiments/deploy/scripts/invoke_carbon.bash
+++ experiments/deploy/scripts/invoke_carbon.bash
@@ -17,7 +17,7 @@ export CORE_PEER_TLS_ENABLED=true
 export CORE_PEER_ID=cli
 export CORE_PEER_ADDRESS="${CLUSTER_IP}:${PEER_NODE_PORT}"
 export CORE_PEER_LOCALMSPID="Org1MSP"
-export CORE_PEER_MSPCONFIGPATH="${ORG_DIR}/peerOrganizations/${PEER_ORG}/users/Admin@${PEER_ORG}/msp"
+export CORE_PEER_MSPCONFIGPATH="${ORG_DIR}/peerOrganizations/${PEER_ORG}/users/User1@${PEER_ORG}/msp"
 export CORE_PEER_TLS_ROOTCERT_FILE="${ORG_DIR}/peerOrganizations/${PEER_ORG}/peers/peer0.${PEER_ORG}/tls/ca.crt"
 export ORDERER_ADDRESS="${CLUSTER_IP}:${ORDERER_NODE_PORT}"
 export ORDERER_TLS_CA="${ORG_DIR}/ordererOrganizations/${ORDERER_ORG}/orderers/orderer0.${ORDERER_ORG}/tls/ca.crt"
@@ -29,4 +29,4 @@ peer chaincode invoke \
   -C "${CHANNEL}" -n carbon \
   --peerAddresses "${CORE_PEER_ADDRESS}" \
   --tlsRootCertFiles "${CORE_PEER_TLS_ROOTCERT_FILE}" \
-  -c '{"function":"CheckCredAttr","Args":["admin"]}'
+  -c '{"function":"CheckCredAttr","Args":["price_viewer"]}'
