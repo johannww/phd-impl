7f130f36999cccec65e4b3012f443ededb2a139e
Johann Westphall <johannwestphall@gmail.com>
2026-03-06 17:23:03 -0300

    Fetch invoke vars from ./experiments/deploy/helm/values.yaml

--- experiments/deploy/scripts/invoke_carbon.bash
+++ experiments/deploy/scripts/invoke_carbon.bash
@@ -3,13 +3,13 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 ORG_DIR="${ORG_DIR:-${SCRIPT_DIR}/../vars/organizations}"
+VALUES_FILE="${SCRIPT_DIR}/../helm/values.yaml"
 
-PEER_ORG="org1"
-ORDERER_ORG="org2"
-CHANNEL="carbon"
-# NodePort values must match values.yaml nodePortBase
-PEER_NODE_PORT=30060
-ORDERER_NODE_PORT=30050
+FIRST_PEER_ORG=$(yq e '.network.organizations[] | select(.peers) | .name' $VALUES_FILE | head -1)
+ORDERER_ORG=$(yq e '.network.organizations[] | select(.orderers) | .name' $VALUES_FILE | head -1)
+CHANNEL=$(yq e '.network.channelName' $VALUES_FILE)
+PEER_NODE_PORT=$(yq e ".network.organizations[] | select(.name == \"${FIRST_PEER_ORG}\") | .nodePortBase" $VALUES_FILE)
+ORDERER_NODE_PORT=$(yq e ".network.organizations[] | select(.name == \"${ORDERER_ORG}\") | .nodePortBase" $VALUES_FILE)
 
 CLUSTER_IP="$(minikube ip)"
 
@@ -17,8 +17,8 @@ export CORE_PEER_TLS_ENABLED=true
 export CORE_PEER_ID=cli
 export CORE_PEER_ADDRESS="${CLUSTER_IP}:${PEER_NODE_PORT}"
 export CORE_PEER_LOCALMSPID="Org1MSP"
-export CORE_PEER_MSPCONFIGPATH="${ORG_DIR}/peerOrganizations/${PEER_ORG}/users/User1@${PEER_ORG}/msp"
-export CORE_PEER_TLS_ROOTCERT_FILE="${ORG_DIR}/peerOrganizations/${PEER_ORG}/peers/peer0.${PEER_ORG}/tls/ca.crt"
+export CORE_PEER_MSPCONFIGPATH="${ORG_DIR}/peerOrganizations/${FIRST_PEER_ORG}/users/User1@${FIRST_PEER_ORG}/msp"
+export CORE_PEER_TLS_ROOTCERT_FILE="${ORG_DIR}/peerOrganizations/${FIRST_PEER_ORG}/peers/peer0.${FIRST_PEER_ORG}/tls/ca.crt"
 export ORDERER_ADDRESS="${CLUSTER_IP}:${ORDERER_NODE_PORT}"
 export ORDERER_TLS_CA="${ORG_DIR}/ordererOrganizations/${ORDERER_ORG}/orderers/orderer0.${ORDERER_ORG}/tls/ca.crt"
 
