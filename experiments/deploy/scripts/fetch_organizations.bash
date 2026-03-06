36b95b64814c235b1dd574798745b9728eb0c446
Johann Westphall <johannwestphall@gmail.com>
2026-03-06 04:36:44 -0300

    wait for pod to be on

--- experiments/deploy/scripts/fetch_organizations.bash
+++ experiments/deploy/scripts/fetch_organizations.bash
@@ -8,9 +8,14 @@ LOCAL_DIR="${LOCAL_DIR:-${SCRIPT_DIR}/../vars/organizations}"
 
 # Pick the first peer org's peer-0 pod via label selector
 PEER_ORG="$(yq e '.network.organizations[] | select(.peers) | .name' "${VALUES_FILE}" | head -1)"
-POD="$(kubectl -n "${NAMESPACE}" get pod \
-  -l "app.kubernetes.io/component=peer,app.kubernetes.io/org=${PEER_ORG},app.kubernetes.io/node-id=peer-0" \
-  -o jsonpath='{.items[0].metadata.name}')"
+POD=""
+while [[ -z "${POD}" ]]; do
+  echo "Waiting for peer pod to be ready..."
+  POD="$(kubectl -n "${NAMESPACE}" get pod \
+    -l "app.kubernetes.io/component=peer,app.kubernetes.io/org=${PEER_ORG},app.kubernetes.io/node-id=peer-0" \
+    -o jsonpath='{.items[0].metadata.name}')"
+  sleep 2
+done
 
 mkdir -p "${LOCAL_DIR}"
 echo "Copying /organizations from pod ${POD} (${NAMESPACE}) to ${LOCAL_DIR}..."
