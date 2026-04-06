f7819e703edc7660e9e264d0bb22218369b531cc
Johann Westphall <johannwestphall@gmail.com>
2026-04-06 18:43:42 -0300

    decode base64 fields for block

--- experiments/debug/block.sh
+++ experiments/debug/block.sh
@@ -3,6 +3,17 @@
 BLOCK_NUM="$1"
 
 peer channel fetch -c carbon ${BLOCK_NUM} /dev/stdout 2>/dev/null \
-  | configtxlator proto_decode --type common.Block \
-| jq --indent 2 'walk(if type == "object" then with_entries(select(.key != "endorser" and .key != "id_bytes")) else . end)'
-
+| configtxlator proto_decode --type common.Block \
+| jq '
+  walk(
+    if type=="object"
+    then with_entries(select(.key!="endorser" and .key!="id_bytes"))
+    else .
+    end
+  )
+' \
+| jq '
+  .data.data[].payload.data.actions[].payload.action.proposal_response_payload.extension.response.payload |= (@base64d | try fromjson catch .)
+  |
+  .data.data[].payload.data.actions[].payload.chaincode_proposal_payload.input.chaincode_spec.input.args |= map(@base64d)
+' --indent 2
