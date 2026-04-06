3f5d2c90dc287dd8cd7c02115708f4fa555e0630
Johann Westphall <johannwestphall@gmail.com>
2026-04-06 18:04:22 -0300

    remove verbose endorser cert and id_bytes to focus on transactions

--- experiments/debug/block.sh
+++ experiments/debug/block.sh
@@ -4,5 +4,5 @@ BLOCK_NUM="$1"
 
 peer channel fetch -c carbon ${BLOCK_NUM} /dev/stdout 2>/dev/null \
   | configtxlator proto_decode --type common.Block \
-  | jq --indent 2
+| jq --indent 2 'walk(if type == "object" then with_entries(select(.key != "endorser" and .key != "id_bytes")) else . end)'
 
